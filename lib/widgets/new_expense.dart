c6a6b7d848a1ccfc43717243881afa0baae270fb
Raphael Pinel <Raphael.Pinel@knowit.fi>
2023-12-21 17:46:29 +0200

    Adjust padding for keyboard in landscape

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -98,104 +98,113 @@ class _NewExpenseState extends State<NewExpense> {
 
   @override
   Widget build(BuildContext context) {
+    final keyboardInsertedContent = MediaQuery.of(context).viewInsets.bottom;
+
     var showDateError =
         _selectedDate == null && _hasBeenSubmitted && !_isDateValid;
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(20, 50, 20, 20),
-      child: Form(
-        key: _formKey,
-        child: Column(
-          // crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            TextFormField(
-              controller: _titleController,
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              decoration: const InputDecoration(label: Text('Title')),
-              maxLength: 50,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter a title';
-                }
-                return null;
-              },
-            ),
-            Row(
+
+    return SizedBox(
+      height: double.infinity,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding:
+              EdgeInsets.fromLTRB(20, 50, 20, keyboardInsertedContent + 20),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              // crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                SizedBox(
-                  width: 100,
-                  child: TextFormField(
-                    
-                    keyboardType: TextInputType.number,
-                    controller: _amountController,
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    decoration: const InputDecoration(
-                        label: Text('Amount'), suffixText: '€'),
-                    onChanged: (value) {
-                      // Add your onChanged logic here
-                    },
-                    validator: _validateAmount,
-                  ),
+                TextFormField(
+                  controller: _titleController,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  decoration: const InputDecoration(label: Text('Title')),
+                  maxLength: 50,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a title';
+                    }
+                    return null;
+                  },
                 ),
-                const SizedBox(width: 10),
-                Expanded(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        showDateError
-                            ? 'Please select a date'
-                            : _selectedDate == null
-                                ? 'No date Selected'
-                                : formatter.format(_selectedDate!),
-                        style: TextStyle(
-                          color: showDateError
-                              ? Theme.of(context).colorScheme.error
-                              : null,
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 100,
+                      child: TextFormField(
+                        keyboardType: TextInputType.number,
+                        controller: _amountController,
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        decoration: const InputDecoration(
+                            label: Text('Amount'), suffixText: '€'),
+                        onChanged: (value) {
+                          // Add your onChanged logic here
+                        },
+                        validator: _validateAmount,
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            showDateError
+                                ? 'Please select a date'
+                                : _selectedDate == null
+                                    ? 'No date Selected'
+                                    : formatter.format(_selectedDate!),
+                            style: TextStyle(
+                              color: showDateError
+                                  ? Theme.of(context).colorScheme.error
+                                  : null,
+                            ),
+                          ),
+                          IconButton(
+                              icon: const Icon(Icons.calendar_month),
+                              color: showDateError
+                                  ? Theme.of(context).colorScheme.error
+                                  : null,
+                              onPressed: _presentDatePicker)
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Expanded(
+                      child: DropdownButtonFormField<CategoryType>(
+                        value: _selectedCategory,
+                        style: Theme.of(context).textTheme.bodyMedium,
+                        onChanged: (newValue) {
+                          if (newValue == null) {
+                            return;
+                          }
+                          setState(() {
+                            _selectedCategory = newValue;
+                          });
+                        },
+                        items: _categoryOptionsItems,
+                        decoration: const InputDecoration(
+                          labelText: 'Category',
                         ),
                       ),
-                      IconButton(
-                          icon: const Icon(Icons.calendar_month),
-                          color: showDateError
-                              ? Theme.of(context).colorScheme.error
-                              : null,
-                          onPressed: _presentDatePicker)
-                    ],
-                  ),
+                    ),
+                    const SizedBox(width: 16),
+                    TextButton(
+                        onPressed: _closeModal, child: const Text('Cancel')),
+                    ElevatedButton(
+                      onPressed: _submitForm,
+                      child: const Text('Add Expense'),
+                    ),
+                  ],
                 )
               ],
             ),
-            const SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Expanded(
-                  child: DropdownButtonFormField<CategoryType>(
-                    value: _selectedCategory,
-                    style: Theme.of(context).textTheme.bodyMedium,
-                    onChanged: (newValue) {
-                      if (newValue == null) {
-                        return;
-                      }
-                      setState(() {
-                        _selectedCategory = newValue;
-                      });
-                    },
-                    items: _categoryOptionsItems,
-                    decoration: const InputDecoration(
-                      labelText: 'Category',
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 16),
-                TextButton(onPressed: _closeModal, child: const Text('Cancel')),
-                ElevatedButton(
-                  onPressed: _submitForm,
-                  child: const Text('Add Expense'),
-                ),
-              ],
-            )
-          ],
+          ),
         ),
       ),
     );
